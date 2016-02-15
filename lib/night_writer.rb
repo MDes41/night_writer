7715f1a3a84a4c84049fffeff64f75a8d8c9035e
Matt DesMarteau <MDes41@users.noreply.github.com>
2016-02-15 16:13:27 -0700

    refactor with ali

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -11,82 +11,143 @@ class NightWriter
 
   def print_output
     finalized_input_string
-    "#{line1}\n#{line2}\n#{line3}"
-  end
-
-  def delete_new_lines
-    input.gsub!("\n"," ")
-  end
-
-  def index_the_capitals
-    index_the_capitals = []
-    input.chars.each_with_index do |chars, index|
-      if ("A".."Z").include?(chars)
-        index_the_capitals << index
-      end
+    output = ''
+    arranged_lines_in_array.each do |line|
+      output += line + "\n"
     end
-    index_the_capitals
-  end
-
-  def star_the_capitals_and_downcase
-    index_the_capitals.reverse.each do |index|
-      input[index] = input[index].downcase
-      input.insert(index, "*")
-    end
-    input
-  end
-
-  def braille_wrap
-    [@braille_word_top.join.scan(/.{1,80}/), @braille_word_mid.join.scan(/.{1,80}/), @braille_word_bot.join.scan(/.{1,80}/)].transpose
-  end
-
-  def index_the_numbers
-    index_the_numbers = []
-    valid_num = (0..9).to_a.map(&:to_s)
-    input.chars.each_with_index do |chars, index|
-      if valid_num.include?(chars)
-        if index == 0
-          index_the_numbers << index
-        elsif valid_num.include?(input.chars[index - 1]) == false
-          index_the_numbers << index
-        end
-      end
-    end
-    index_the_numbers
-  end
-
-  def place_pound_at_numbers
-    index_the_numbers.reverse.each do |index|
-      input.insert(index, "#")
-    end
-    input
+    output = output.chomp
+    "#{output}"
   end
 
   def finalized_input_string
     delete_new_lines
     star_the_capitals_and_downcase
     place_pound_at_numbers
+    mark_where_breaks_should_be
   end
 
-  def line1
+  def delete_new_lines
+    input.gsub!("\n"," ")
+  end
+
+  # def index_the_numbers
+  #   index_the_numbers = []
+  #   valid_num = (0..9).to_a.map(&:to_s)
+  #   input.chars.each_with_index do |chars, index|
+  #     if valid_num.include?(chars)
+  #       if index == 0
+  #         index_the_numbers << index
+  #       elsif valid_num.include?(input.chars[index - 1]) == false
+  #         index_the_numbers << index
+  #       end
+  #     end
+  #   end
+  #   index_the_numbers
+  # end
+  # def index_the_capitals
+  #   index_the_capitals = []
+  #   input.chars.each_with_index do |chars, index|
+  #     if ("A".."Z").include?(chars)
+  #       index_the_capitals << index
+  #     end
+  #   end
+  #   index_the_capitals
+  # end
+  # def index_the_spaces
+  #   index_the_spaces = []
+  #   input.chars.each_with_index do |chars, index|
+  #     if chars == " "
+  #       index_the_spaces << index
+  #     end
+  #   end
+  #   index_the_spaces
+  # end
+
+  def index_the_capitals
+    input.chars.map.with_index do |char, index|
+      index if ("A".."Z").include?(char)
+    end.compact
+  end
+
+
+  def star_the_capitals_and_downcase
+    index_the_capitals.reverse.each do |index|
+      input[index] = input[index].downcase
+      input.insert(index, "*")
+    end
+  end
+
+  def index_the_numbers
+    input.chars.map.with_index { |char,index| index if char =~ /[\d]/ &&
+    (input[index - 1] =~ /[^\d]/ || index == 0) }.compact
+  end
+
+  def place_pound_at_numbers
+    index_the_numbers.reverse.each do |index|
+      input.insert(index, "#")
+    end
+  end
+
+
+  def index_the_spaces
+    input.chars.map.with_index do |chars, index|
+      index if chars == " "
+    end.compact
+  end
+
+  def line_wrap_index_arr
+    line_wrap_index_arr = []
+    wrap = 40
+    index_the_spaces.each_with_index do |num, index|
+      if wrap - num <= 0
+        previous_index = index_the_spaces[index - 1]
+        line_wrap_index_arr << previous_index
+        wrap = previous_index + 40
+      end
+    end
+  line_wrap_index_arr
+  end
+
+  def mark_where_breaks_should_be
+    line_wrap_index_arr.reverse.each do |index|
+      input.insert(index + 1, "~")
+    end
+    input
+  end
+
+  def each_line
+    input.split("~")
+  end
+
+  def arranged_lines_in_array
+    arranged_lines_in_array = []
+    each_line.each do |line|
+      arranged_lines_in_array << line1(line)
+      arranged_lines_in_array << line2(line)
+      arranged_lines_in_array << line3(line)
+    end
+    arranged_lines_in_array
+  end
+
+  def line1(line)
     line1 = []
-    input.chars.each do |letter|
+    line.chars.each do |letter|
       line1 << braille_map[letter][0..1]
     end
     line1.join
   end
 
-  def line2
+  def line2(line)
     line2 = []
-    input.chars.each do |letter|
+    line.chars.each do |letter|
       line2 << braille_map[letter][2..3]
     end
     line2.join
   end
 
-  def line3
+  def line3(line)
     line3 = []
-    input.chars.each do |letter|
+    line.chars.each do |letter|
       line3 << braille_map[letter][4..5]
     end
     line3.join
--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -6,30 +6,35 @@ require_relative '../lib/night_writer'
 class NightWriterTest < Minitest::Test
 
   def test_that_the_letter_a_is_printed_in_braille
+    # skip
     nw = NightWriter.new("a")
     output = "0.\n..\n.."
     assert_equal output, nw.print_output
   end
 
   def test_hello_world_line1_is_printed
+    # skip
     nw = NightWriter.new("hello world")
     line1 = "0.0.0.0.0....00.0.0.00"
-    assert_equal line1, nw.line1
+    assert_equal line1, nw.line1("hello world")
   end
 
   def test_hello_world_line2_is_printed
+    # skip
     nw = NightWriter.new("hello world")
     line2 = "00.00.0..0..00.0000..0"
-    assert_equal line2, nw.line2
+    assert_equal line2, nw.line2("hello world")
   end
 
   def test_hellow_world_line3_is_printed
+    # skip
     nw = NightWriter.new("hello world")
     line3 = "....0.0.0....00.0.0..."
-    assert_equal line3, nw.line3
+    assert_equal line3, nw.line3("hello world")
   end
 
   def test_hello_world_is_on_three_lines
+    # skip
     nw = NightWriter.new("hello world")
     line1 = "0.0.0.0.0....00.0.0.00"
     line2 = "00.00.0..0..00.0000..0"
@@ -39,21 +44,26 @@ class NightWriterTest < Minitest::Test
   end
 
   def test_input_to_array_wo_newl
+    # skip
     nw = NightWriter.new("delete\nnew\nline\nchars\n")
     assert_equal "delete new line chars ", nw.delete_new_lines
   end
 
   def test_index_the_capitals_gets_array_of_where_capitals_are
+    # skip
     nw = NightWriter.new("Hello World")
     assert_equal [0,6], nw.index_the_capitals
   end
 
   def test_star_the_capitals_and_downcase
+    # skip
     nw = NightWriter.new("Hello World")
-    assert_equal ("*hello *world"), nw.star_the_capitals_and_downcase
+    nw.star_the_capitals_and_downcase
+    assert_equal ("*hello *world"), nw.input
   end
 
   def test_capital_is_put_in_proper_braille
+    # skip
     nw = NightWriter.new("H")
     line1 = "..0."
     line2 = "..00"
@@ -68,11 +78,14 @@ class NightWriterTest < Minitest::Test
   end
 
   def test_number_adds_the_pound_sign_only_once
+    # skip
     nw = NightWriter.new("79 7 7 79990 9")
-    assert_equal "#79 #7 #7 #79990 #9", nw.place_pound_at_numbers
+    nw.place_pound_at_numbers
+    assert_equal "#79 #7 #7 #79990 #9", nw.input
   end
 
   def test_pound_sign_added_and_in_proper_braille
+    # skip
     nw = NightWriter.new("7 79")
     line1 = ".000...000.0"
     line2 = ".000...0000."
@@ -80,4 +93,47 @@ class NightWriterTest < Minitest::Test
     output = "#{line1}\n#{line2}\n#{line3}"
     assert_equal output, nw.print_output
   end
+
+  def test_index_the_spaces
+    # skip
+    sample = (('a'..'z').to_a*8).join.gsub('z',' ')
+    nw = NightWriter.new(sample)
+    assert_equal [25,51,77,103,129,155,181,207], nw.index_the_spaces
+  end
+
+  def test_line_wrap_index_arr
+    # skip
+    sample = (('a'..'z').to_a*8).join.gsub('z',' ')
+    nw = NightWriter.new(sample)
+    nw.index_the_spaces
+    assert_equal [25, 51, 77, 103, 129, 155, 181], nw.line_wrap_index_arr
+  end
+
+  def test_mark_where_breaks_should_be
+    # skip
+    sample = (('a'..'z').to_a*8).join.gsub('z',' ')
+    nw = NightWriter.new(sample)
+    nw.index_the_spaces
+    output = nw.line_wrap_index_arr
+    array_of_tildas = nw.mark_where_breaks_should_be.chars
+    array_of_tildas.find_all.with_index do |char, index|
+      return index if char == "~"
+    end
+    assert_equal output, array_of_tildas
+  end
+
+  def test_each_line
+    # skip
+    nw = NightWriter.new("first line~second line")
+    assert_equal ["first line", "second line"], nw.each_line
+  end
+
+  def test_arrange_lines_in_array
+    # skip
+    nw = NightWriter.new("h~h")
+    output = ['0.','00','..','0.','00','..']
+    assert_equal output, nw.arranged_lines_in_array
+  end
+
+
 end
